7a70c3da800fd8473520685c2ce24b39f1df326d
Aniel-Feyt <anielfeyt@gmail.com>
2021-05-21 15:03:40 +0200

    Refactored

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,8 @@ const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, sequi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, veritatis facilis possimus voluptate similique itaque, ipsam velit, molestias perspiciatis blanditiis tempore! Commodi sapiente assumenda laborum blanditiis laudantium ut numquam similique distinctio? Aliquid eius, deserunt, perferendis ea facere, natus culpa error atque repellat ex officia quam rerum fugiat facilis necessitatibus et.</p>
         </Layout>
 
     );
