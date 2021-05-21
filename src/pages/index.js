7ba4d6c73feade53e41fe746ececd56845c72629
Aniel-Feyt <anielfeyt@gmail.com>
2021-05-21 13:33:39 +0200

    Added h2 and text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,10 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutotial.</p>
       <p>I must say I am almost impress with the results so far.</p>
+      <p>Please tell me that this will work.</p>
+
+      <h2>I love Gatsby!</h2>
+      <p>Some more text.</p>
     </Layout>
 
   );
